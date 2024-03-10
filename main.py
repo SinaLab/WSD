7d9c03271d7a0da4558f12802c006cacfe35c46c
alaa-aljabari <155227590+alaa-aljabari@users.noreply.github.com>
2024-03-10 11:56:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -134,8 +134,8 @@ def GlossPredictor(target, example, glosses):
     return 'none','none'
 
 
-filePath = 'WSD_Accuracy/Data/Test.json' #'./ArabGlossBERT/examples.json'
-glossesDict= "WSD_Accuracy/Data/dictionary.json" #"./ArabGlossBERT/dictionary.json"
+filePath = 'WSD/Data/Test.json' #'./ArabGlossBERT/examples.json'
+glossesDict= "WSD/Data/dictionary.json" #"./ArabGlossBERT/dictionary.json"
 glossesDictContent = {}
 with open(filePath, 'r', encoding='utf-8') as file:
    fileContent = file.read()
